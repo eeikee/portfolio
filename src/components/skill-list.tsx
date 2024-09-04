042090a5b572237329f2f89a48f34599c89e959d
ekonuma <ekonuma12@gmail.com>
2024-09-04 15:27:31 +0900

    feat: skill list updated

--- src/components/skill-list.tsx
+++ src/components/skill-list.tsx
@@ -13,16 +13,26 @@ export default function SkillList() {
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Skills</h1>
 
-      <Card>
-        <Card.Grid style={gridStyle}>Content</Card.Grid>
-        <Card.Grid hoverable={false} style={gridStyle}>
-          Content
-        </Card.Grid>
-        <Card.Grid style={gridStyle}>Content</Card.Grid>
-        <Card.Grid style={gridStyle}>Content</Card.Grid>
-        <Card.Grid style={gridStyle}>Content</Card.Grid>
-        <Card.Grid style={gridStyle}>Content</Card.Grid>
-        <Card.Grid style={gridStyle}>Content</Card.Grid>
+      <Card title="Backend">
+        <Card.Grid style={gridStyle}>Java</Card.Grid>
+        <Card.Grid style={gridStyle}>Spring Framework</Card.Grid>
+        <Card.Grid style={gridStyle}>Golang</Card.Grid>
+      </Card>
+      <Card title="Frontend">
+        <Card.Grid style={gridStyle}>Javascript</Card.Grid>
+        <Card.Grid style={gridStyle}>Typescript</Card.Grid>
+        <Card.Grid style={gridStyle}>React</Card.Grid>
+        <Card.Grid style={gridStyle}>Next Js</Card.Grid>
+      </Card>
+      <Card title="Cloud">
+        <Card.Grid style={gridStyle}>Azure</Card.Grid>
+        <Card.Grid style={gridStyle}>Azure DevOps</Card.Grid>
+        <Card.Grid style={gridStyle}>AWS</Card.Grid>
+      </Card>
+      <Card title="Infra and others">
+        <Card.Grid style={gridStyle}>Docker</Card.Grid>
+        <Card.Grid style={gridStyle}>Kubernetes </Card.Grid>
+        <Card.Grid style={gridStyle}>Github</Card.Grid>
       </Card>
     </section >
   )
