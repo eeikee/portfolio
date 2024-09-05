a878f8040ff13f9a6d604760b0c8b594d5496c57
ekonuma <ekonuma12@gmail.com>
2024-09-05 15:46:32 +0900

    incomplete: skills list type change to tags

--- src/components/skill-list.tsx
+++ src/components/skill-list.tsx
@@ -1,39 +1,113 @@
-'use client';
+"use client";
 
-import { Card } from 'antd';
-import React from 'react';
+import { Tag } from "antd";
+import React from "react";
 
 const gridStyle: React.CSSProperties = {
-  width: '25%',
-  textAlign: 'center',
+  width: "25%",
+  textAlign: "center",
 };
+
+const skills = [
+  "Java",
+  "Spring",
+  "Golang",
+  "Javascript",
+  "Typescript",
+  "React",
+  "TypeScript",
+  "Next JS",
+  "Azure",
+  "AWS",
+  "Azure DevOps",
+  "Docker",
+  "Kubernetes",
+  "Github",
+  "Gitlab",
+];
+
+const category = [
+  {
+    name: "Java",
+    type: "backend"
+  },
+  { name: "Spring",
+    type: "backed"
+  },
+{
+  name: "Golang",
+  type: "backend"
+},
+{
+  name: "Javascript",
+  type: "backend"
+},
+{ name: "Typescript",
+  type: "frontend"
+},
+{
+  name: "React",
+  type: "frontend"
+},
+{
+name:  "Next JS",
+type: "frontend"
+},
+{
+ name:  "Azure",
+ type: "cloud"
+},
+{
+ name: "AWS",
+type: "cloud"
+},
+{
+  name: "Azure DevOps",
+  type: "service"
+},
+{
+  name: "Github",
+  type: "service"
+},
+{
+  name: "Gitlab",
+  type: "service"
+},
+{
+  name: "Docker",
+  type: "Infra"
+},
+{
+  name: "Kubernetes",
+  type: "Infra"
+}
+];
+
+const categories = new Map();
+
+categories.set("backend", "orange");
+categories.set("frontend", "  geekblue");
+categories.set("infra", "purple");
+categories.set("cloud", "cyan");
+categories.set("service", "lime");
 
 export default function SkillList() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Skills</h1>
 
-      <Card title="Backend">
-        <Card.Grid style={gridStyle}>Java</Card.Grid>
-        <Card.Grid style={gridStyle}>Spring Framework</Card.Grid>
-        <Card.Grid style={gridStyle}>Golang</Card.Grid>
-      </Card>
-      <Card title="Frontend">
-        <Card.Grid style={gridStyle}>Javascript</Card.Grid>
-        <Card.Grid style={gridStyle}>Typescript</Card.Grid>
-        <Card.Grid style={gridStyle}>React</Card.Grid>
-        <Card.Grid style={gridStyle}>Next Js</Card.Grid>
-      </Card>
-      <Card title="Cloud">
-        <Card.Grid style={gridStyle}>Azure</Card.Grid>
-        <Card.Grid style={gridStyle}>Azure DevOps</Card.Grid>
-        <Card.Grid style={gridStyle}>AWS</Card.Grid>
-      </Card>
-      <Card title="Infra and others">
-        <Card.Grid style={gridStyle}>Docker</Card.Grid>
-        <Card.Grid style={gridStyle}>Kubernetes </Card.Grid>
-        <Card.Grid style={gridStyle}>Github</Card.Grid>
-      </Card>
-    </section >
-  )
+      <div>
+        {categories.entries.map((key,color) => (
+          <Tag key={key} color={color|}>
+            {skill}
+          </Tag>
+        ))}
+        {skills.map((skill) => (
+          <Tag key={skill} color="blue">
+            {skill}
+          </Tag>
+        ))}
+      </div>
+    </section>
+  );
 }
