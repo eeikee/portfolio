f047eeea687061a3bdf289ea7eab67839e8869d5
Eike Konuma <ekonuma12@gmail.com>
2024-12-15 00:38:26 +0900

    fix: wrong image at orp project

--- data/Projects.ts
+++ data/Projects.ts
@@ -14,13 +14,13 @@ export const projects: Project[] = [
       tecnologies: [skills.Java, skills.Android],
       description: "ORP ou Organizador Rápido de Pedidos é um aplicativo Android que permite a rápida comunicação entre o pedido em um restaurante e seu processamento na cozinha.",
       links: ["https://github.com/ekonuma/ORP-Organizador-Rapido-de-Pedidos"],
-      cover: "https://avatars.githubusercontent.com/u/80468144?s=200&v=4",
+      cover: "",
     }, {
       name: "OGZ",
       description: "OGZ é um programa desktop especialmente criado para facilitar a organização do estoque de materiais utilizados pelo grupo Omega de robótica da Facens (Faculdade de Engenharia de Sorocaba).",
       tecnologies: [skills.Java],
       links: ["https://github.com/Omeganizer"],
-      cover: "",
+      cover: "https://avatars.githubusercontent.com/u/80468144?s=200&v=4",
     }, {
       name: "Smartcash",
       description: "Smartcash é um aplicativo de controle financeiro e e controle de vendas para Android.",
