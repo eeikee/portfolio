3510e7d05ada80b50eec97069962267b03a16844
ekonuma <ekonuma12@gmail.com>
2025-02-03 11:52:05 +0900

    feat: Add more projects

--- data/Projects.ts
+++ data/Projects.ts
@@ -27,5 +27,41 @@ export const projects: Project[] = [
       tecnologies: [skills.Java, skills.Android],
       links: ["https://github.com/JEAM-Team"],
       cover: "",
+    }, {
+      name: "Microservices",
+      description: "Criação de microserviços utilizando Spring Framework e Java, orientação a eventos e mensageria.",
+      tecnologies: [skills.Java, skills.Spring],
+      links: [],
+      cover: "",
+    }, {
+      name: "Upgrade from Java 8 Spring 2 to Java 17 Spring 3",
+      description: "Atualização da implementação do Spring 2 para Spring 3 juntamente com a atualização do Java 8 para Java 17.",
+      tecnologies: [skills.Java, skills.Spring],
+      links: [],
+      cover: "",
+    }, {
+      name: "Upgrade Azure SDK for Java",
+      description: "Upgrade das libs do Azure SDK para Java como Eventhub, Cosmos DB, StorageAccount entre outros.",
+      tecnologies: [skills.Java, skills.Azure],
+      links: [],
+      cover: "",
+    }, {
+      name: "Azure DevOps pipelines",
+      description: "Criação de pipelines de CI/CD para os projetos no Azure DevOps.",
+      tecnologies: [skills.Azure,skills.AzureDevOps],
+      links: [],
+      cover: "",
+    }, {
+      name: "Shell script automation",
+      description: "Criação de scripts shell para automatização de tarefas e pipelines.",
+      tecnologies: [skills.Shell],
+      links: [],
+      cover: "",
+    }, {	
+      name: "Functions",
+      description: "Criação de funções serverless utilizando Java e Spring",
+      tecnologies: [skills.Java, skills.Spring, skills.Azure],
+      links: ["https://github.com/JEAM-Team"],
+      cover: "",
     }
   ];
